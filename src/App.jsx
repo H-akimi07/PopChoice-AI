562de465018fd913be8f1736ddc68725622ad098
https://github.com/H-akimi07 <hakimisameera56@gmail.com>
2026-09-09 19:23:27 +0430

    tailwinf is working test deleted

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,6 @@ import QuestionsView from "./components/QuestionsView";
 function App() {
   return (
     <div className="min-h-screen bg-black text-white">
-      <div className="p-10 text-4xl font-bold text-purple-400">
-        Tailwind is working
-      </div>
-
       <QuestionsView />
     </div>
   );
